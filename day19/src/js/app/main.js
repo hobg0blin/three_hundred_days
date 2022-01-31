435a06f1f5446d92e795e0caa202698409a3432f
Brent <bb2906@nyu.edu>
2022-01-31 15:30:32 -0500

    shadows

---
+++ day19/src/js/app/main.js
@@ -0,0 +1,125 @@
+// Global imports -
+import * as THREE from 'three';
+import {BufferGeometryUtils} from 'three/examples/jsm/utils/BufferGeometryUtils.js'
+
+import {createCamera} from '../components/Three/camera.js'
+import {createLights} from '../components/Three/lights.js'
+import {createRenderer} from '../components/Three/renderer.js'
+import { createFog} from '../components/Three/fog.js'
+import {createControls} from '../components/Three/controls.js'
+import randomColor from 'randomcolor'
+
+const clock = new THREE.Clock()
+//FIXME: still using global state variable, need to refactor MIDI stuff
+const currentKeys = {}
+
+// This class instantiates and ties all of the components together, starts the loading process and renders the main loop
+export default class Texture {
+    constructor() {
+        // set up scene
+        // CAMERA
+        // fov, aspect, near, far
+      this.camera = createCamera()
+//      this.camera.lookAt(new THREE.Vector3(5000,200, 20000))
+        // SCENE & RENDER
+        this.renderer = createRenderer()
+        this.scene = new THREE.Scene();
+
+        //LIGHTS
+        const color = 0xFFFFFF
+        const intensity = 3
+        this.light = createLights(color, intensity)
+        console.log('light: ', this.light)
+        this.scene.add(this.light[0].target)
+        this.scene.add(this.light[0], new THREE.AmbientLight(color, 0))
+        this.scene.add(this.light[1])
+        //BACKGROUND & FOG
+        let backgroundColor = new THREE.Color(0xa2c1f2)
+        this.scene.background = backgroundColor
+//        this.scene.fog = createFog(0xffea4d, 15, 2000)
+        // CONTROLS
+        createControls(this.camera, this.renderer, this.light[0])
+        //GROUND
+        const ground = new THREE.Mesh(new THREE.PlaneGeometry(100, 100, 1, 1), new THREE.MeshPhongMaterial({ color: 0x4287f5, shininess: 30, emissiveIntensity: 10}))
+        ground.rotation.x = - Math.PI /2
+        ground.receiveShadow = true
+        this.scene.add(ground)
+        // shapes & textures
+
+        this.keyboard = []
+        this.buildKeyboard = this.buildKeyboard.bind(this)
+        this.buildKeyboard()
+//        this.camera.lookAt(this.keyboard[18])
+        this.defaultPos = new THREE.BufferAttribute()
+        this.judder = this.judder.bind(this)
+        this.render = this.render.bind(this) //bind to class instead of window object
+
+    }
+    buildKeyboard() {
+        const box = new THREE.BoxGeometry(1, 4, 1)
+
+        for (let i = 0; i <= 36; i++) {
+            const boxMat = new THREE.MeshPhongMaterial({color: new THREE.Color(randomColor())})
+            const boxMesh = new THREE.Mesh(box, boxMat)
+            boxMesh.position.set(-15 + 0.6*(i+1), 0, 0)
+            boxMesh.castShadow = true
+            this.scene.add(boxMesh)
+            this.keyboard.push(boxMesh)
+        }
+        navigator.requestMIDIAccess().then((midi) => {
+
+            startLoggingMIDIInput(midi, this.doJudder)
+            console.log('midi supported')
+        })
+        function onMIDIMessage( event) {
+            if (event.data[0] == 144 && event.data[2] > 0 ) {
+                //subtract 48 bc thats minimum key code
+                currentKeys[event.data[1]] = 'on'
+            }
+            if (event.data[2] == 0) {
+                console.log('note off: ', event.data)
+//                keyboard[event.data[1] - 48].scale(1)
+                currentKeys[event.data[1]] = 'off'
+            }
+//            console.log('current keys: ', currentKeys)
+           }
+
+    function startLoggingMIDIInput( midiAccess) {
+        for (var input of midiAccess.inputs.values()) {
+        input.onmidimessage = onMIDIMessage;
+        }
+        }
+    }
+    judder(shape) {
+        shape.scale.y += 0.05
+
+    }
+    render(time, i) {
+
+        this.renderer.setAnimationLoop(() => {
+        const xSpd = time * 0.15
+        for (let key in currentKeys) {
+            if (currentKeys[key] == 'on') {
+                this.judder(this.keyboard[key - 48])
+            }
+            if (currentKeys[key] == 'off') {
+                this.keyboard[key - 48].scale.y -= 0.05
+                if (this.keyboard[key-48].scale.y == 1){
+           delete currentKeys[key]
+                }
+            }
+        }
+        //TODO: update to proper animation loop per https://discoverthreejs.com/book/first-steps/animation-loop/#timing-in-the-animation-system
+
+        console.log('ca0mera pos: ', this.camera.position)
+
+//        this.text.material.needsUpdate = true
+//        this.train.rotation.y += xSpd
+//          this.camera.position.x -= xSpd
+//          this.camera.position.z -= xSpd
+//          this.camera.position.y -= xSpd/2
+//        this.text.rotation.x += xSpd
+        this.renderer.render(this.scene, this.camera)
+})
+}
+}
---
+++ day19/webpack.config.js
@@ -0,0 +1,98 @@
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack')
+const CopyPlugin = require('copy-webpack-plugin')
+module.exports = (env) => {
+  console.log('ENVIRONMENT: ', env)
+  let mode = "development"
+  if (env.NODE_ENV === 'prod') {
+    devtool = 'hidden-source-map'
+    mode = 'production'
+    stats = 'none'
+  }
+  return {
+    entry: path.join(__dirname, 'src/js', 'app.js'),
+    mode: mode,
+    output: {
+      path: path.resolve(__dirname, './dist'),
+      filename: 'bundle.js'
+    },
+    plugins: [
+/* FAILED ATTEMPT TO COPY FONTS TO DIST
+ * new CopyPlugin({
+      patterns:
+        [{from: './node_modules/three/examples/fonts/', to: "./dist/fonts/"}]}), */
+      new webpack.DefinePlugin({
+        __ENV__: JSON.stringify(env.NODE_ENV)
+      }),
+
+      new MiniCssExtractPlugin({
+        filename: '[name].bundle.css',
+        chunkFilename: '[id].css'
+      }),
+      new webpack.HotModuleReplacementPlugin(),
+  new HtmlWebpackPlugin({
+        title: 'Three.js Webpack ES6 Boilerplate',
+        template: path.join(__dirname, 'src/html/index.html'),
+        path: path.resolve(__dirname, './dist'),
+        filename: 'index.html'
+  })
+    ],
+    devServer: {
+      open: true,
+      static: './src/public/',
+      hot: true,
+      headers: {
+      "Access-Control-Allow-Origin": "*",
+     "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+        "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+      }
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(jsx|js)$/,
+          include: path.resolve(__dirname, 'src'),
+          exclude: /node_modules/,
+          use: [{
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                ['@babel/preset-env', {
+                  "targets": {
+                    "node": "12"
+                  }
+                }],
+                '@babel/preset-react'
+              ]
+            }
+          }]
+        },
+        {
+          test: /\.css$/i,
+          include: path.resolve(__dirname, 'src'),
+          exclude: /node_modules/,
+          use: [
+            'style-loader',
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap:true
+              }
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                postcssOptions: {
+                  config: "./postcss.config.js"
+                }
+              }
+            }
+          ]
+        },
+      ]
+    },
+  }
+}
+
