e6c7510c19bb247e881d726877b5953576785d55
Brent <bb2906@nyu.edu>
2022-02-25 16:20:09 -0500

    heightfield stuff

---
+++ day44/src/js/components/Three/camera.js
@@ -0,0 +1,12 @@
+import { PerspectiveCamera } from 'three'
+//TODO actually add args/make this good
+function createCamera() {
+    const camera = new PerspectiveCamera(65, window.innerWidth / window.innerHeight, 0.1, 20000);
+        camera.position.set(5, 70 , 55)
+   camera.lookAt(0, -5, 50)
+
+//    camera.zoom = 1.5
+    return camera
+}
+export {createCamera}
+
